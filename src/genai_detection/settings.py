f73a3020fff4b57f40da22bd7f20696cbdbd9d54
Paweł Grabiński <pawelrgrabinski@gmail.com>
2024-03-23 22:18:17 +0100

    Set seed everywhere (#3)
    * Add constant random seed everywhere
    
    * Update poetry lock
    
    * Add "no space left" in Actions workarounds
    
    * Update Action name
    
    * Add additional diskspace cleaning during Build Image job
    
    * Add wandb and tqdm
    
    * Customize download test dataset job run name
    
    * Update docker actions versions
    
    * Change the action for freeing disk space
    
    * Remove doubled dependencies in pyproject.toml and update poetry.lock
    
    * Add poetry dependencies managment to download_dataset job
    
    * Correct poetry usage

--- src/genai_detection/settings.py
+++ src/genai_detection/settings.py
@@ -48,3 +48,18 @@ with open("config.yaml") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
 CONFIG_VERSION = config["version"]
+
+def seed_everything(seed: int):
+    import random, os
+    import numpy as np
+    import torch
+    
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False 
+
+seed_everything(config["seed"])
