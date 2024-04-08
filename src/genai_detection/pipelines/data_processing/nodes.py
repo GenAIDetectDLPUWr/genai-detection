bf9fcd117f35cfbf85c139de47132f66797ea914
Pawel Grabinski <pawelrgrabinski@gmail.com>
2024-04-08 17:27:13 +0000

    Add train and test datasets

--- src/genai_detection/pipelines/data_processing/nodes.py
+++ src/genai_detection/pipelines/data_processing/nodes.py
@@ -19,7 +19,7 @@ def transform_raw_image_data(raw_image_dataset):
 
 preprocess_raw_image_data_node = node(
                 func=transform_raw_image_data,
-                inputs="image_test_data",
+                inputs="image_train_data",
                 outputs="preprocessed_data",
                 name="raw_image_data_preprocessing",
             )
