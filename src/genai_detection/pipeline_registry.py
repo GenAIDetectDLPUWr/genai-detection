0ceeef04fc870a9410709db01b6f972f5840d7bc
Pawel Grabinski <pawelrgrabinski@gmail.com>
2024-05-06 21:08:18 +0000

    Add multipile piepline handling

--- src/genai_detection/pipeline_registry.py
+++ src/genai_detection/pipeline_registry.py
@@ -4,6 +4,8 @@ from typing import Dict
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
 
+from genai_detection.pipelines import evaluation, training
+
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -11,6 +13,9 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
-    pipelines = find_pipelines()
+    pipelines = {
+        "training": training.create_pipeline(),
+        "evaluation": evaluation.create_pipeline(),
+    }
     pipelines["__default__"] = sum(pipelines.values())
     return pipelines
