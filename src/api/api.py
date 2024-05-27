2dfad9c4cd656f22df3617e199d257c03346d54a
Pawel Grabinski <pawelrgrabinski@gmail.com>
2024-05-27 23:45:07 +0000

    Add model download and loading utilities for api

--- src/api/api.py
+++ src/api/api.py
@@ -2,8 +2,15 @@ from fastapi import FastAPI, UploadFile, File
 from PIL import Image
 import numpy as np
 
+from src.utils.model import download_model_from_reqistry, load_model
+from src.utils.training import create_run
+from src.utils.config import API_CONFIG
+
 app = FastAPI()
 
+model_run = create_run({"goal": "download_model"})
+model_path = download_model_from_reqistry(model_run, API_CONFIG["model_name"])
+model = load_model(model_path)
 
 @app.post("/inference")
 async def inference(file: UploadFile = File(...)):
--- src/utils/model.py
+++ src/utils/model.py
@@ -4,6 +4,9 @@ import wandb
 from wandb.sdk.wandb_run import Run
 from pathlib import Path
 import os
+import torch
+import numpy as np
+from torchvision import models, transforms
 
 from training import create_run
 from config import API_CONFIG
@@ -29,3 +32,26 @@ def download_model_from_reqistry(run: Run, model_name: str, version: str = "late
         os.rename(downloaded_model_path, destination_path)
 
     return destination_path
+
+def load_model(model_path: str):
+    """
+    Loads the model's state dict from a file.
+    
+    Parameters:
+    model_path (str): The path to the model file.
+    """
+    # Consider dynamic class typing
+    model = models.resnet18()
+    for param in model.parameters():
+        param.requires_grad = False
+    num_features = model.fc.in_features
+    model.fc = torch.nn.Linear(num_features, 1)
+    model.load_state_dict(torch.load(model_path))
+    return model
+
+def model_inference(model, image_array: np.ndarray):
+    model.eval()
+    logit = model(image_array)
+    prediction = torch.sigmoid(logit)
+
+    return {"prediction": prediction.item()}
