a0e42f2547823716c2582acda2e74c10370a3d8c
Pawel Grabinski <pawelrgrabinski@gmail.com>
2024-05-31 09:47:16 +0000

    Add working API

--- src/api/api.py
+++ src/api/api.py
@@ -2,19 +2,18 @@ from fastapi import FastAPI, UploadFile, File
 from PIL import Image
 import numpy as np
 
-from utils.model import download_model_from_reqistry, load_model
-from utils.training import create_run
+from utils.model import download_model_from_reqistry, load_model, model_inference
 from utils.config import API_CONFIG
 
 app = FastAPI()
 
-model_run = create_run({"goal": "download_model"})
-model_path = download_model_from_reqistry(model_run, API_CONFIG["model_name"])
+model_path = download_model_from_reqistry(API_CONFIG["model_name"])
 model = load_model(model_path)
 
 @app.post("/inference")
 async def inference(file: UploadFile = File(...)):
     image = Image.open(file.file)
     image_array = np.array(image)
+    prediction = model_inference(model, image_array)
 
-    raise NotImplementedError("You need to add inference pipeline here!")
+    return {"prediction": prediction}
